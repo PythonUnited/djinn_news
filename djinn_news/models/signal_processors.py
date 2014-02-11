13ee84aea5c77299a87eef2493aa3d41575224f6
Martin Bouma <bouma@egoactive.com>
2014-02-11 16:38:26 +0100

    provgron-1659

--- djinn_news/models/signal_processors.py
+++ djinn_news/models/signal_processors.py
@@ -1,6 +1,7 @@
 import logging
+from datetime import datetime
 from notification import models as notification
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from pgauth.models import UserGroup
 from pgevents.events import Events
@@ -8,7 +9,7 @@ from news import News
 
 
 LOGGER = logging.getLogger("djinn_news")
-
+NEW_NEWS = "new_news"
 
 @receiver(post_save, sender=News)
 def post_save_news(sender, instance, **kwargs):
@@ -25,14 +26,24 @@ def post_save_news(sender, instance, **kwargs):
         except:
             LOGGER.exception("Couldn't send notification for news")
 
+
+@receiver(pre_save, sender=News)
+def news_pre_save(sender, instance, **kwargs):
+
+    if instance.id and instance.title and instance.publish_from and \
+            instance.publish_from <= datetime.now():
+        # We do not create activities when title is empty
+
         context = {
             "content_item": instance,
             "title": instance.title,
             "url": instance.get_absolute_url(),
             "parentusergroup": instance.parentusergroup}
 
-        Events.send(
-            "new_news",
-            user=instance.changed_by,
-            context=context,
-            usergroup=instance.parentusergroup)
+        if not instance.publish_notified:
+            Events.send(
+                NEW_NEWS,
+                user=instance.changed_by,
+                context=context,
+                usergroup=instance.parentusergroup)
+            instance.publish_notified = True
