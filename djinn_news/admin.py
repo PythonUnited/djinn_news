6081a939031e3712b887b4f35c75c6a72055c293
Martin Bouma <bouma@egoactive.com>
2014-03-13 12:28:39 +0100

    extra admin features

--- djinn_news/admin.py
+++ djinn_news/admin.py
@@ -3,8 +3,10 @@ from models.news import News
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', )
-    search_fields = ['title']
+    # eigenaar, publicatie datum en de publiceren tot datum
+    list_display = ('title', 'changed_by', 'get_owner', 'publish_from', 'publish_to', 'is_global')
+    list_filter = ['publish_from', 'is_global']
+    search_fields = ['title', 'changed_by__userprofile__name', 'text']
 
 
 admin.site.register(News, NewsAdmin)
