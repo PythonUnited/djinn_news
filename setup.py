0738426771714df7af785103a521daf5c0d14065
Martin Bouma <bouma@egoactive.com>
2015-03-18 15:03:20 +0100

    version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
     ]
 
 setup(name='djinn_news',
-      version="1.1.0",
+      version="1.1.1",
       description='Djinn Intranet News',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
