a9bf187bc504b3e777280567316f6ce9ccf9dff1
Jaqueline Berghout <jaqueline@geekaboo.nl>
2015-04-22 13:48:31 +0200

    iframe url aangepast zodat z-index deze ook pakt

--- djinn_news/static/js/djinn_news.js
+++ djinn_news/static/js/djinn_news.js
@@ -1,1 +1,9 @@
 /* JS for Djinn news */
+
+$('iframe').each( function() {
+    var url = $(this).attr("src")
+    $(this).attr({
+        "src" : url.replace('?rel=0', '')+"?wmode=transparent",
+        "wmode" : "Opaque"
+    })
+});
