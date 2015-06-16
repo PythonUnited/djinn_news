be0b07c434636bc6f62f320ac37ba7eb2e801334
Jaqueline Berghout <jaqueline@geekaboo.nl>
2015-06-16 12:49:05 +0200

    verwijderd van ?wmode=transparent, afgesproken dat Jan B dit zelf gaat doen

--- djinn_news/static/js/djinn_news.js
+++ djinn_news/static/js/djinn_news.js
@@ -1,10 +1,1 @@
 /* JS for Djinn news */
-$(document).ready(function () {
-    $('iframe').each(function () {
-        var url = $(this).attr("src")
-        $(this).attr({
-            "src": url.replace('?rel=0', '') + "?wmode=transparent",
-            "wmode": "Opaque"
-        })
-    });
-});
