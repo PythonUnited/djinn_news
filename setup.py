f11809da8b0b5a73a8ae3733fe3655e3d5581584
Martin Bouma <bouma@egoactive.com>
2015-09-29 11:28:23 +0200

    preparing release 1.1.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     ]
 
 setup(name='djinn_news',
-      version="1.1.3",
+      version="1.1.4",
       description='Djinn Intranet News',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
