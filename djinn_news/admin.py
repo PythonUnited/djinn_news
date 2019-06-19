15d11ff1313927bc1b15a1689fbb810d5fa8be3b
Martin Bouma <martin@studioreactor.com>
2019-06-19 11:08:18 +0200

    raw_id_fields in admin

--- djinn_news/admin.py
+++ djinn_news/admin.py
@@ -4,9 +4,11 @@ from .models.news import News
 
 class NewsAdmin(admin.ModelAdmin):
     # eigenaar, publicatie datum en de publiceren tot datum
+    filter_horizontal = ['images']
     list_display = ('title', 'changed_by', 'get_owner', 'publish_from',
                     'publish_to', 'is_global', 'is_sticky', 'publish_for_feed')
     list_filter = ['publish_from', 'is_global', 'publish_for_feed']
+    raw_id_fields = ['creator', 'changed_by', 'parentusergroup', 'home_image']
     search_fields = ['title', 'changed_by__userprofile__name', 'text']
 
     readonly_fields = ['highlight_from']
