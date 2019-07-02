24f26739eb98f24f95357213d9ce21e1e80ed5ee
Martin Bouma <martin@studioreactor.com>
2019-07-02 12:18:41 +0200

    enable raw wysiwyg content to be put into narrowcasting field

--- djinn_news/forms/news.py
+++ djinn_news/forms/news.py
@@ -32,7 +32,7 @@ class NewsForm(BaseContentForm, RelateMixin, RichTextMixin):
         required=True,
         widget=RichTextWidget(
             img_type="djinn_contenttypes.ImgAttachment",
-            attrs={'class': 'extended', 'name': 'text'}
+            attrs={'class': 'extended description_feed_src', 'name': 'text'}
         ))
 
     documents = RelateField(
