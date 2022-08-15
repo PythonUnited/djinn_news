88fa7c3c7ef9a6a9370190bf1d10711ffb228f95
Martin Bouma <martin@studioreactor.com>
2022-08-15 15:51:51 +0200

    home_image weer aangezet voor 'Evenementen'

--- djinn_news/forms/news.py
+++ djinn_news/forms/news.py
@@ -148,7 +148,7 @@ class NewsForm(DjinnCroppingMixin, BaseContentForm, RelateMixin, RichTextMixin):
 
         if self.initial.get('category_slug') == News.CATEGORY_OCCURRENCE or (
                 self.instance.category and self.instance.category.slug == News.CATEGORY_OCCURRENCE):
-            del self.fields['home_image']
+            # del self.fields['home_image']
             del self.fields['is_sticky']
         else:
             del self.fields['event_dt']
